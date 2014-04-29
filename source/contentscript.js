a1a4adc6f1f5a073aff16c69eaeeb16abf5d21f3
cotne nazarashvili <cotne.95@gmail.com>
2014-04-29 15:53:25 +0400

    open files from all urls

--- source/contentscript.js
+++ source/contentscript.js
@@ -4,21 +4,21 @@ if (meta_remove_path && meta_remove_path.content) {
 }else{
 	var remove_path = false;
 }
-
 var open_file = document.getElementsByClassName('open_file');
-
-for (var key = 0; key < open_file.length; key++) {
-	open_file[key].addEventListener('click', function(e) {
-		e.preventDefault()
-		if (this.dataset.openfile) {
-			var filePath = this.dataset.openfile.trim();
-			if (remove_path) {
-				filePath = filePath.replace(remove_path,'');
+if (open_file.length) {
+	for (var key = 0; key < open_file.length; key++) {
+		open_file[key].addEventListener('click', function(e) {
+			e.preventDefault();
+			if (this.dataset.openfile) {
+				var filePath = this.dataset.openfile.trim();
+				if (remove_path) {
+					filePath = filePath.replace(remove_path,'');
+				};
+				chrome.runtime.sendMessage({
+					command: "open",
+					filePath: filePath
+				}/* [response] */);
 			};
-			chrome.runtime.sendMessage({
-				command: "open",
-				filePath: filePath
-			}/* [response] */);
-		};
-	});
-}
+		});
+	};
+};
