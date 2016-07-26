662beac16ff607dff2cc9964a9e75d0618518738
thecotne <cotne@itdc.ge>
2016-07-26 11:11:04 +0400

    eslint

--- js/optionsPage.js
+++ js/optionsPage.js
@@ -1,24 +1,27 @@
-/* global chrome */
-/* eslint no-underscore-dangle: 0 */
+/* global chrome, storage */
 
-
-const optionElems = document.querySelectorAll('[data-options]')
+const optionElems = document.querySelectorAll('[data-options]');
 
 for (const elem of optionElems) {
   switch (elem.type) {
-    case 'text':
-      elem.addEventListener('change', e => {
-        storage[e.target.name] = e.target.value;
-      });
-
-      storage[elem.name].then(value => elem.value = value);
-      break;
-    case 'checkbox':
+    case 'checkbox': {
       elem.addEventListener('change', e => {
         storage[e.target.name] = e.target.checked;
       });
 
-      storage[elem.name].then(checked => elem.checked = checked);
+      storage[elem.name].then(checked => {
+        elem.checked = checked;
+      });
       break;
+    }
+    default: {
+      elem.addEventListener('change', e => {
+        storage[e.target.name] = e.target.value;
+      });
+
+      storage[elem.name].then(value => {
+        elem.value = value;
+      });
+    }
   }
 }
--- js/storage.js
+++ js/storage.js
@@ -1,16 +1,23 @@
+/* global chrome, storage */
+/* eslint no-unused-vars: 0 */
+
 const storageType = 'local';
 
 function getDefault(key) {
   switch (key) {
-    case 'localPath':
+    case 'localPath': {
       return '\\\\192.168.1.33\\local\\';
+    }
+    default: {
+      return undefined;
+    }
   }
 }
 
 class storage {
   static get localPath() {
     return new Promise((resolve, reject) => {
-      chrome.storage[storageType].get('localPath', ({localPath}) => {
+      chrome.storage[storageType].get('localPath', ({ localPath }) => {
         resolve(localPath || getDefault('localPath'));
       });
     });
@@ -19,12 +26,3 @@ class storage {
     chrome.storage[storageType].set({ localPath });
   }
 }
-
-
-
-
-
-
-
-
-
